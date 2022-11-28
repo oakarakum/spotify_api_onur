9ea4215bcc756d47df3f1d328a76fb4892b1141f
Onur A. Karakum <113251720+oakarakum@users.noreply.github.com>
2022-11-28 18:56:06 +0300

    dev updated

--- lib/src/features/widgets/browse_textformfield.dart
+++ lib/src/features/widgets/browse_textformfield.dart
@@ -1,6 +1,8 @@
-import 'package:animate_do/animate_do.dart';
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:spotify_api_onur/src/ui/pages/search_screen.dart';
 
 class BrowseTextformfield extends StatefulWidget {
   const BrowseTextformfield({super.key});
@@ -12,35 +14,36 @@ class BrowseTextformfield extends StatefulWidget {
 class _BrowseTextformfieldState extends State<BrowseTextformfield> {
   @override
   Widget build(BuildContext context) {
-    return FadeIn(
-      child: SizedBox(
-        height: 5.3.h,
-        child: TextFormField(
-          style: TextStyle(color: Color(0xffFAFAFA)),
-          textAlign: TextAlign.start,
-          textAlignVertical: TextAlignVertical.center,
-          decoration: InputDecoration(
-              contentPadding: EdgeInsets.all(0),
-              //isDense: true,
-              fillColor: Colors.green,
-              filled: true,
-              prefixIcon: IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.search),
-                color: Colors.white,
-                iconSize: 3.h,
-              ),
-              prefixIconColor: Colors.white,
-              hintText: "Ne dinlemek istiyorsun?",
-              hintStyle: TextStyle(
-                  color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
-              enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                  borderRadius: BorderRadius.circular(5)),
-              focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                  borderRadius: BorderRadius.circular(5))),
-        ),
+    return SizedBox(
+      height: 5.3.h,
+      child: TextFormField(
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => SearchList()));
+        },
+        style: TextStyle(color: Color(0xffFAFAFA)),
+        textAlign: TextAlign.start,
+        textAlignVertical: TextAlignVertical.center,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(0),
+            fillColor: Colors.green,
+            filled: true,
+            prefixIcon: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.search),
+              color: Colors.white,
+              iconSize: 3.h,
+            ),
+            prefixIconColor: Colors.white,
+            hintText: "Ne dinlemek istiyorsun?",
+            hintStyle: TextStyle(
+                color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+                borderRadius: BorderRadius.circular(5)),
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+                borderRadius: BorderRadius.circular(5))),
       ),
     );
   }
